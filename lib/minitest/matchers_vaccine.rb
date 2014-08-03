c6d8ac9e140ac53c057b658985edd1cc1211495d
Ryan McGeary <ryan@mcgeary.org>
2014-08-03 13:00:41 -0400

    Added #assert_must, #assert_wont, #must, and #wont

--- lib/minitest/matchers_vaccine.rb
+++ lib/minitest/matchers_vaccine.rb
@@ -1,7 +1,86 @@
 require "minitest/matchers_vaccine/version"
 
-module Minitest
-  module MatchersVaccine
-    # Your code goes here...
+# Borrowed and modified from minitest-matchers, but we don't need all the
+# other RSpec-style expectation "infections."
+module MiniTest
+  module Assertions
+    ##
+    # Passes if matcher.matches?(subject) returns true
+    #
+    # Example:
+    #
+    #   def test_validations
+    #     assert_must be_valid, @user
+    #   end
+    def assert_must(matcher, subject, msg = nil)
+      msg = message(msg) do
+        if matcher.respond_to? :failure_message
+          matcher.failure_message # RSpec 3.x, 1.1
+        else
+          matcher.failure_message_for_should # RSpec 2.x, 1.2
+        end
+      end
+
+      assert matcher.matches?(subject), msg
+    end
+
+    ##
+    # Facilitator to assert_must for use with minitest-spec. If no subject
+    # given, defaults to matching against the current `subject` or the
+    # instance variable `@subject`.
+    #
+    # Example:
+    #
+    #   subject { Order.new }
+    #
+    #   it "should have associations" do
+    #     must belong_to :account
+    #     must have_many :line_items
+    #   end
+    def must(matcher, subject = @subject || subject, msg = nil)
+      assert_must matcher, subject, msg
+    end
+
+    ##
+    # Passes if matcher.matches?(subject) returns false
+    #
+    # Example:
+    #
+    #   def test_validations
+    #     assert_wont be_valid, @user
+    #   end
+    def assert_wont(matcher, subject, msg = nil)
+      msg = message(msg) do
+        if matcher.respond_to? :failure_message_when_negated # RSpec 3.x
+          matcher.failure_message_when_negated # RSpec 3.x
+        elsif matcher.respond_to? :failure_message_for_should_not
+          matcher.failure_message_for_should_not # RSpec 2.x, 1.2
+        else
+          matcher.negative_failure_message # RSpec 1.1
+        end
+      end
+
+      if matcher.respond_to? :does_not_match?
+        assert matcher.does_not_match?(subject), msg
+      else
+        refute matcher.matches?(subject), msg
+      end
+    end
+
+    ##
+    # Facilitator to assert_wont for use with minitest-spec. If no subject
+    # given, defaults to matching against the current `subject` or the
+    # instance variable `@subject`.
+    #
+    # Example:
+    #
+    #   subject { User.new }
+    #
+    #   it "should validate" do
+    #     wont have_valid(:email).when("foo", "foo@bar", "@bar.com")
+    #   end
+    def wont(matcher, subject = @subject || subject, msg = nil)
+      assert_wont matcher, subject, msg
+    end
   end
 end
---
+++ test/matchers_test.rb
@@ -0,0 +1,131 @@
+require "minitest/autorun"
+require "minitest/matchers_vaccine"
+
+module InstanceOf
+  def be_instance_of klass
+    Matcher.new klass
+  end
+
+  class Matcher
+    def initialize klass
+      @klass = klass
+    end
+
+    def description
+      "be instance of #{@klass}"
+    end
+
+    def matches? subject
+      @subject = subject
+      @subject.instance_of? @klass
+    end
+
+    def failure_message
+      "expected to " + description
+    end
+
+    def failure_message_when_negated
+      "expected not to " + description
+    end
+  end
+end
+
+describe "#assert_must" do
+  include InstanceOf
+
+  it "should be capable of passing" do
+    assert_equal true, assert_must(be_instance_of(Array), [:hello])
+    assert_equal true, assert_must(be_instance_of(String), "hello")
+  end
+
+  it "should be capable of failing" do
+    assert_raises(Minitest::Assertion) { assert_must(be_instance_of(String), [:hello]) }
+    assert_raises(Minitest::Assertion) { assert_must(be_instance_of(Array), "hello") }
+  end
+end
+
+describe "#assert_wont" do
+  include InstanceOf
+
+  it "should be capable of passing" do
+    assert_equal false, assert_wont(be_instance_of(String), [:hello])
+    assert_equal false, assert_wont(be_instance_of(Array), "hello")
+  end
+
+  it "should be capable of failing" do
+    assert_raises(Minitest::Assertion) { assert_wont(be_instance_of(Array), [:hello]) }
+    assert_raises(Minitest::Assertion) { assert_wont(be_instance_of(String), "hello") }
+  end
+end
+
+describe "#must" do
+  include InstanceOf
+
+  describe "given a subject" do
+    subject { [:hello] }
+
+    it "should be capable of passing" do
+      assert_equal true, must(be_instance_of(Array))
+    end
+
+    it "should be capable of failing" do
+      assert_raises(Minitest::Assertion) { must be_instance_of String }
+    end
+  end
+
+  describe "given a @subject" do
+    before do
+      @subject = "hello"
+    end
+
+    it "should be capable of passing" do
+      assert_equal true, must(be_instance_of(String))
+    end
+
+    it "should be capable of failing" do
+      assert_raises(Minitest::Assertion) { must be_instance_of Array }
+    end
+  end
+
+  describe "without a subject" do
+    it "should error" do
+      assert_raises(NameError) { must be_instance_of Array }
+    end
+  end
+end
+
+describe "#wont" do
+  include InstanceOf
+
+  describe "given a subject" do
+    subject { [:hello] }
+
+    it "should be capable of passing" do
+      assert_equal false, wont(be_instance_of(String))
+    end
+
+    it "should be capable of failing" do
+      assert_raises(Minitest::Assertion) { wont be_instance_of Array }
+    end
+  end
+
+  describe "given a @subject" do
+    before do
+      @subject = "hello"
+    end
+
+    it "should be capable of passing" do
+      assert_equal false, wont(be_instance_of(Array))
+    end
+
+    it "should be capable of failing" do
+      assert_raises(Minitest::Assertion) { wont be_instance_of String }
+    end
+  end
+
+  describe "without a subject" do
+    it "should error" do
+      assert_raises(NameError) { wont be_instance_of Array }
+    end
+  end
+end
